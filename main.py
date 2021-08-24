5312ca4b0e0e41e99494c068925ec698ebcb5bb7
Donika Fetahu <fetahudonike@gmail.com>
2021-08-24 14:23:11 +0200

    testing my function

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a + b
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Donika')
     print('How are you')
     print('Let us do a second commit')
+    print(my_function(2, 4))
 
 
