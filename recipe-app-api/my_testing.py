b71716cf0d8801031c5c4d7c2ae9326fa867d935
Donika Fetahu <fetahudonike@gmail.com>
2021-08-24 14:31:19 +0200

    added my function my_function

--- recipe-app-api/my_testing.py
+++ recipe-app-api/my_testing.py
@@ -3,6 +3,11 @@
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a + b
+
+print(my_function(2, 4))
+
     # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Donika')
